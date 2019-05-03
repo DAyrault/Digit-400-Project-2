a6c9aeeb65b282de2526e9f4652b0ff1fa06031a
David Ayrault <silvermarksman@gmail.com>
2019-05-03 23:37:28 +0000

    updated style

--- content.py
+++ content.py
@@ -2,13 +2,9 @@ def Content():
     APP_CONTENT = {
         "Home":[["Welcome","/welcome/","Welcome"],
                ["About","/about/","About"],
-               ["Messages","/messages/","Messages"],],
+               ["Search","/search/","Search"],],
         "Profile":[["User Profile","/profile/","User Profile"],
                   ["Settings","/settings/","Settings"],
                   ["Terms of Service","/tos/","Terms of Service"],],
-        "Search":[["Search","/search/","Search"],
-                 ["Recent Activity","/recent/","Recent Activity"],],
-        "Messages":[["Messages","/messages/","Messages"],
-                   ["Alerts","/alerts/","Alerts"]],
     }
     return APP_CONTENT
